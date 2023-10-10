b40664a6adbd0eff5e02df818cf38dd51ff87a9b
cleemansen <github-action@actions-template-sync.noreply.github.com>
2023-10-10 09:27:16 +0000

    chore(template): merge template changes :up:

--- utils/input_converter.py
+++ utils/input_converter.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import movingpandas as mpd
+from tests.config.definitions import ROOT_DIR
 
 
 class InputConverter:
@@ -8,7 +9,7 @@ class InputConverter:
     def csv_to_pickle(self, csv_path, result_file_name):
         print(os.getcwd())
 
-        pandas = self.read_data_csv(file_path=f'{csv_path}/buffer.csv')
+        pandas = self.read_data_csv(file_path=f'{csv_path}/link.csv')
         timezone = self.read_timezone(file_path=f'{csv_path}/meta.csv')
         projection = self.read_projection(file_path=f'{csv_path}/meta.csv')
 
@@ -19,7 +20,7 @@ class InputConverter:
     def read_data_csv(self, file_path):
         csv = pd.read_csv(
             file_path,
-            parse_dates=['timestamp'],
+            parse_dates=['timestamps'],
         )
         print(csv.info())
         return csv
@@ -35,14 +36,14 @@ class InputConverter:
         return projection
 
     def adjust_timestamps(self, data, timezone):
-        data['timestamp_tz'] = data['timestamp'].apply(lambda x: x.tz_localize(timezone))
+        data['timestamp_tz'] = data['timestamps'].apply(lambda x: x.tz_localize(timezone))
         print('applied timezone', timezone)
         print(data.head())
 
     def create_moving_pandas(self, data, projection):
         move = mpd.TrajectoryCollection(
             data,
-            traj_id_col='individual.local.identifier',
+            traj_id_col='trackId',
             crs=projection,
             t='timestamp_tz',  # use our converted timezone column
             x='location.long',
@@ -58,7 +59,9 @@ class InputConverter:
 
 if __name__ == '__main__':
     converter = InputConverter()
-    converter.csv_to_pickle(
-        csv_path="./resources/input/input4",
-        result_file_name="./resources/input/input4/converted.pickle"
-    )
+    for i in range(1,5):
+        file = f'input{i}'
+        converter.csv_to_pickle(
+            csv_path=f'./resources/input/{file}',
+            result_file_name=f'{ROOT_DIR}/resources/samples/{file}.pickle'
+        )
